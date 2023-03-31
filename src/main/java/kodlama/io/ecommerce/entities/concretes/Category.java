365798722ec493181b45a18ada73fc4c06d7818c
Kubra Celik <kubracelikce@gmail.com>
2023-04-01 01:01:32 +0300

    update

---
+++ src/main/java/kodlama/io/ecommerce/entities/concretes/Category.java
@@ -0,0 +1,20 @@
+package kodlama.io.ecommerce.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
---
+++ src/main/java/kodlama/io/ecommerce/entities/enums/Status.java
@@ -0,0 +1,7 @@
+package kodlama.io.ecommerce.entities.enums;
+
+public enum Status {
+    AVAILABLE,
+    UNAVAILABLE,
+    IN_PROGRESS
+}
